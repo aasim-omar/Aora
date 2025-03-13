13f5d4e561cf89c02352a7eba06ef07830080619
aasim-omar <aasim.omar7@gmail.com>
2025-03-13 23:36:28 +0200

    Add search functionality

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,11 +1,47 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, FlatList, Image } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import useAppwrite from "../../lib/useAppwrite";
+import { searchPosts } from "../../lib/appwrite";
+import VideoCard from "../../components/VideoCard";
+import EmptyState from "../../components/EmptyState";
+import SearchInput from "../../components/SearchInput";
+import { images } from "../../constants";
+import { useEffect } from "react";
 
 const Search = () => {
+
+  const { query } = useLocalSearchParams()
+  const { data: posts, refetch } = useAppwrite(() => searchPosts(query))
+  
+  useEffect(() => {
+    refetch();
+  }, [query])
+
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <SafeAreaView className="bg-primary text-white h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({item}) => (<VideoCard item={item}/>)}
+        ListHeaderComponent={() => (
+          <View className="my-6 mx-4">
+            <View className="flex justify-between flex-row my-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">Search Results For the Query:</Text>
+                <Text className="font-psemibold text-2xl text-white">{query}</Text>
+              </View>
+              <View>
+                <Image source={images.logoSmall} className="w-9 h-10" resizeMode="contain"/>
+              </View>
+            </View>
+            <SearchInput initialQuery={query}/>
+          </View>
+        )}
+        ListEmptyComponent={() => <EmptyState title="No Viedios Founds" subtitle="No Videos found fro this search Query" />}
+        // refreshControl={<RefreshControl onRefresh={onRefresh} refreshing={refreshing} />}
+      />
+    </SafeAreaView>
   );
 };
 
--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,20 +1,40 @@
-import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Image, Alert } from "react-native";
 import { icons } from "../constants";
-import { useState } from "react"
+import { useState } from "react";
+import { router, usePathname } from "expo-router";
 
-export default function SearchInput() {
-	const [ focus, setFocus ] = useState(false);
+export default function SearchInput({ initialQuery }) {
+  const [focus, setFocus] = useState(false);
+  const pathname = usePathname();
+  const [query, setQuery] = useState(initialQuery || "");
   return (
-    <View className={`flex flex-row items-center space-x-4 w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 ${focus ? "border-secondary" : ""}`}>
+    <View
+      className={`flex flex-row items-center space-x-4 w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 ${
+        focus ? "border-secondary" : ""
+      }`}
+    >
       <TextInput
-        className="text-base mt-0.5 text-white flex-1 font-pregular"
+        className="mt-0.5 text-white flex-1 font-pregular"
         placeholder="Search a video topic"
         placeholderTextColor="#CDCDE0"
-				onFocus={() => setFocus(true)}
-				onBlur={() => setFocus(false)}
+        value={query}
+        onChangeText={(e) => setQuery(e)}
+        onFocus={() => setFocus(true)}
+        onBlur={() => setFocus(false)}
       />
 
-      <TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          if (!query) {
+            return Alert.alert("Empty Search Field", "Please Enter Something To Search");
+          }
+          if (pathname.startsWith("/search")) {
+            router.setParams({query})
+          } else {
+            router.push(`/search/${query}`)
+          }
+        }}
+      >
         <Image source={icons.search} className="w-5 h-5" resizeMode="contain" />
       </TouchableOpacity>
     </View>
